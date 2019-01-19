0f487fbe161d73a43c5682c66e2ead9566f188aa
Phua Yue Jun <whyjayxp@hotmail.com>
2019-01-19 19:08:42 +0800

    Added echo and reps

--- ParrotBot.py
+++ ParrotBot.py
@@ -1,14 +1,27 @@
 import requests
 token = "722519073:AAGz5JXntidyedz1xhxBCaL4k9XxogGx_Tg"
 url = "https://api.telegram.org/bot" + token + "/"
+reps = 1
+
 while True:
-    result = requests.get(url + "getUpdates?offset=610416947&timeout=1").json()['result']
+    result = requests.get(url + "getUpdates?offset=610416979&timeout=1").json()['result']
     for message in result:
         chat_id = message['message']['chat']['id']
         if 'text' in message['message']:
             reply = message['message']['text']
-            message_out = {"chat_id": chat_id, "text": reply}
-            requests.post(url + "sendMessage", json = message_out)
+            args = reply.split(" ")
+            if args[0] == "/reps":
+                if len(args) == 2:
+                    reps = int(args[1])
+                    message_out = {"chat_id": chat_id, "text": "Echo " + args[1] + " times"}
+                else:
+                    message_out = {"chat_id": chat_id, "text": "Incorrect command"}
+                requests.post(url + "sendMessage", json = message_out)
+            elif args[0] == "/echo":
+                reply = " ".join(args[1:])
+                for i in range(1, reps + 1):
+                    message_out = {"chat_id": chat_id, "text": reply}
+                    requests.post(url + "sendMessage", json = message_out)
         offset = str(message['update_id'] + 1)
         print(offset)
         result = requests.get(url + "getUpdates?offset=" + offset + "&timeout=1").json()['result']
